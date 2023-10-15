d7f42b8ad55ee4698bda3a68acec5a6fe145346c
unknown <489264018@qq.com>
2023-10-15 14:45:38 +0800

    loaders

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -27,6 +27,16 @@ module.exports = {
                     ],
                 },
             }],
+        },{
+            test: /\.(s[ac]ss|css)$/i,
+            use: [
+                // 将 JS 字符串生成为 style 节点
+                'style-loader',
+                // 将 CSS 转化成 CommonJS 模块
+                'css-loader',
+                // 将 Sass 编译成 CSS
+                'sass-loader',
+            ],
         },]
     },
     plugins:[
