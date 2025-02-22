13ce7eb714384b34beb7b4155bd70e572c2395d5
wyq <489264018@qq.com>
2025-02-23 01:34:33 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { rootFontSize, figmaWidth } from './utils/config';
 import style from './App.scss'
 
 function App() {
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      // 监听屏幕宽度变化，动态设置根字体大小，以适配不同屏幕。但是最小宽度为设计稿宽度
+      const scale = Math.max(document.documentElement.clientWidth, figmaWidth) / figmaWidth; // 屏幕宽度 / 设计稿宽度 = 缩放比例，最小为1
+      document.documentElement.style.fontSize = rootFontSize * scale + 'px'; // 1rem = 16px
+      document.documentElement.style.minWidth = figmaWidth + 'px'; // 设置最小宽度为设计稿宽度
+
+      return () => {
+        window.removeEventListener('resize');
+      }
+    })
+  }, []);
+
   return (
     <div className={style.App}>
       测试APP
---
+++ src/utils/config.js
@@ -0,0 +1,4 @@
+export const rootFontSize = 16; // 根据设计稿的基准值设置
+
+export const figmaWidth = 1920; // figma设计稿宽度
+export const figmaHeight = 1080; // figma设计稿高度
