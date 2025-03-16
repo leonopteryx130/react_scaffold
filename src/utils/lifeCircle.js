109667736cbb8511354f6fa7e037ebaf9804653b
wyq <489264018@qq.com>
2025-03-16 13:44:31 +0800

    适配laptop小尺寸

--- src/utils/lifeCircle.js
+++ src/utils/lifeCircle.js
@@ -6,11 +6,18 @@
  * 路由拦截，数据拦截等也可以放在这里边
  * 
  */
-import { rootFontSize, figmaWidth } from '@/utils/config';
+import { rootFontSize, figmaWidth, figmaHeight, mobileFigmaWidth } from '@/utils/config';
+import { isMobile } from "@/utils/commonUtils";
 
 export const initPageLayout = () => {
   // 初始化页面布局
-    const scale = Math.max(document.documentElement.clientWidth, figmaWidth) / figmaWidth; // 屏幕宽度 / 设计稿宽度 = 缩放比例，最小为1
-    document.documentElement.style.fontSize = rootFontSize * scale + 'px'; // 1rem = 16px
-    document.documentElement.style.minWidth = figmaWidth + 'px'; // 设置最小宽度为设计稿宽度
+  const width = isMobile()? mobileFigmaWidth: figmaWidth;
+  const height = isMobile()? document.documentElement.clientHeight: figmaHeight;
+
+  const scaleWidth = Math.max(document.documentElement.clientWidth, 1024) / width;
+  const scaleHeight = Math.max(document.documentElement.clientHeight, 768) / height ;
+  const scale = Math.min(scaleWidth, scaleHeight); // 取最小的缩放比例
+  document.documentElement.style.fontSize = rootFontSize * scale + 'px'; // 1rem = 16px
+  document.documentElement.style.minWidth = width + 'px'; // 设置最小宽度为设计稿宽度
+  document.documentElement.style.minHeight = height + 'px'; // 移动端页面高度
 }
