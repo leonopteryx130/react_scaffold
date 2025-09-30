7bb8fc9d2e7f260e8f533d0928b0b5548edeb2e7
wyq <489264018@qq.com>
2025-09-30 19:39:24 +0800

    自适应缩放丢了

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import RouterView from './router';
 import './index.css';
 import './utilities.scss';
+import { initPageLayout } from '@/utils/lifeCircle';
 
+
+// 初始化自适应缩放
+initPageLayout();
+window.addEventListener('resize', initPageLayout);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
