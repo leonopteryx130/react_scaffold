de1ec0c528e69b15be438fe0ba930cf4a3c9022c
wyq <489264018@qq.com>
2025-09-30 20:01:56 +0800

    自适应宽高

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,4 +3,7 @@ export const rootFontSize = 16; // 根据设计稿的基准值设置
 export const figmaWidth = 1920; // figma设计稿宽度
 export const figmaHeight = 1080; // figma设计稿高度
 
+export const minWidth = 1024 // 页面最小宽度，小于该宽度时，出现滚动条
+export const minHeight = 768 // 页面最小高度，小于该高度时，出现滚动条
+
 export const mobileFigmaWidth = 375; // 移动端figma设计稿宽度
--- src/utils/lifeCircle.js
+++ src/utils/lifeCircle.js
@@ -6,7 +6,7 @@
  * 路由拦截，数据拦截等也可以放在这里边
  * 
  */
-import { rootFontSize, figmaWidth, figmaHeight, mobileFigmaWidth } from '@/utils/config';
+import { rootFontSize, figmaWidth, figmaHeight, mobileFigmaWidth, minWidth, minHeight } from '@/utils/config';
 import { isMobile } from "@/utils/commonUtils";
 
 export const initPageLayout = () => {
@@ -14,10 +14,10 @@ export const initPageLayout = () => {
   const width = isMobile()? mobileFigmaWidth: figmaWidth;
   const height = isMobile()? document.documentElement.clientHeight: figmaHeight;
 
-  const scaleWidth = Math.max(document.documentElement.clientWidth, 1024) / width;
-  const scaleHeight = Math.max(document.documentElement.clientHeight, 768) / height ;
+  const scaleWidth = Math.max(document.documentElement.clientWidth, minWidth) / width;
+  const scaleHeight = Math.max(document.documentElement.clientHeight, minHeight) / height ;
   const scale = Math.min(scaleWidth, scaleHeight); // 取最小的缩放比例
   document.documentElement.style.fontSize = rootFontSize * scale + 'px'; // 1rem = 16px
-  //document.documentElement.style.minWidth = width + 'px'; // 设置最小宽度为设计稿宽度
-  //document.documentElement.style.minHeight = height + 'px'; // 移动端页面高度
+  document.documentElement.style.minWidth = minWidth + 'px'; // 设置最小宽度为设计稿宽度
+  document.documentElement.style.minHeight = minHeight + 'px'; // 移动端页面高度
 }
